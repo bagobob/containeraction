7470f17722e5d307678a74edfa55a3bcff51b9a0
bagobob <htibago@gmail.com>
2023-05-15 14:54:38 +0000

    action created and Dockerfile updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh -l
 
-# make the file executable
-chmod +x entrypoint.sh
-
-#!/bin/sh -l
-
 echo "Hello $1"
 memory =$(cat /proc/meminfo)
-echo $memory
+echo "::set-output name=memory::$memory"
